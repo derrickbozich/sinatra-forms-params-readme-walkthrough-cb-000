e3ab8af908f86302fe2f38bbe53759f19d12841e
derrick b <dbozich1@gmail.com>
2018-07-21 03:58:39 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,5 +7,14 @@ class App < Sinatra::Base
   end
 
   # Add your post route and action below
+  post '/food_form' do
+    @name = params[:name]
+    @food = params[:food]
+  end
+
+  get '/food' do
+    "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
+  end
+  
 
 end
